62fb7a6d9be9e25d9f775f2d0031459150b6431d
Ferdinand <ferdinand.hartmann@web.de>
2025-07-07 15:32:05 +0200

    corrected error and added downsampling to sim launch file

--- obstacle_detection/launch/sim.launch.py
+++ obstacle_detection/launch/sim.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -9,7 +9,18 @@ def generate_launch_description():
     twist_cmd_dir = get_package_share_directory('twist_to_prius_cmd')
     car_control_dir = get_package_share_directory('car_control')
 
+    # Include pointcloud_downsampling launch
+    downsampling_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('pointcloud_downsampling'),
+            'launch',
+            'pointcloud_downsampling.launch.py'
+        ))
+    )
+
     return LaunchDescription([
+        downsampling_launch,
+
         # Launch Gazebo straight road simulation
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
